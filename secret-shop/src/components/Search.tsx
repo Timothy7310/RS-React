338184a689c8d7eed86512c85d2adb8674c6fd9e
Timothy7310 <penlist127@gmail.com>
2023-03-15 16:05:08 +0300

    feat: save value from search bar to LS

--- secret-shop/src/components/Search.tsx
+++ secret-shop/src/components/Search.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import '../styles/search.scss';
 
-class Search extends React.Component {
+type SearchState = {
+  value: string;
+};
+
+type SearchProps = Record<string, never>;
+
+class Search extends React.Component<SearchProps, SearchState> {
+  constructor(props: SearchProps) {
+    super(props);
+
+    this.state = {
+      value: localStorage.getItem('searchValue') ?? '',
+    };
+  }
+
   render() {
     return (
       <>
@@ -10,9 +24,15 @@ class Search extends React.Component {
           name="search"
           className="goods-cards__head-search"
           placeholder="Поиск по названию"
+          onChange={(e) => this.setState({ value: e.target.value })}
+          value={this.state.value}
         />
       </>
     );
+  }
+
+  componentWillUnmount() {
+    localStorage.setItem('searchValue', this.state.value);
   }
 }
 
