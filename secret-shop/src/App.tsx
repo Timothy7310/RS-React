6000a1824bdaf471d515fa3bea375edddade61f0
Timothy7310 <penlist127@gmail.com>
2023-03-16 13:51:39 +0300

    refactor: delete unused code

--- secret-shop/src/App.tsx
+++ secret-shop/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import AboutUsPage from './pages/AboutUsPage';
 import MainPage from './pages/MainPage';
 import NotFoundPage from './pages/NotFoundPage';
