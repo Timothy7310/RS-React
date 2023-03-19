66230599cdd0b4125acc58e20bb8bc04bc86cde2
Timothy7310 <penlist127@gmail.com>
2023-03-19 19:37:27 +0300

    fix: comment out code for app test

--- secret-shop/test/app.test.tsx
+++ secret-shop/test/app.test.tsx
@@ -1,16 +1,16 @@
-// import React from 'react';
-// import { describe, it } from 'vitest';
-// import { render, screen } from '@testing-library/react';
-// import { BrowserRouter } from 'react-router-dom';
-// import App from '../src/App';
+import React from 'react';
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from '../src/App';
 
-// describe('App test', () => {
-//   it('App render', () => {
-//     render(
-//       <BrowserRouter>
-//         <App />
-//       </BrowserRouter>
-//     );
-//     screen.debug();
-//   });
-// });
+describe('App test', () => {
+  it('App render', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    screen.debug();
+  });
+});
