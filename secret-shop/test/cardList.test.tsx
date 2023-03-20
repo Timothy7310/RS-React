c87750c24d33f919e3d5d093ad15f2d9e09eb680
timothy <penlist127@gmail.com>
2023-03-20 16:50:16 +0300

    refactor: remove unused code

--- secret-shop/test/cardList.test.tsx
+++ secret-shop/test/cardList.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import CardList from '../src/components/CardList';
-import { describe, it, expect } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { render } from '@testing-library/react';
 import goods from '../src/assets/goods.json';
 
 describe('CardItem component', () => {
