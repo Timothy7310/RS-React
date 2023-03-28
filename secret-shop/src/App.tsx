2503a7093b2e22514c1283b443664830deee106f
Timothy7310 <penlist127@gmail.com>
2023-03-28 16:33:55 +0300

    refactor: rewrite class component App to func

--- secret-shop/src/App.tsx
+++ secret-shop/src/App.tsx
@@ -11,21 +11,19 @@ import './styles/vendor.scss';
 import './styles/vars.scss';
 import './styles/settings.scss';
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<MainPage page={'Main'} />} />
-            <Route path="about" element={<AboutUsPage page={'About'} />} />
-            <Route path="form" element={<FormPage page={'Form'} />} />
-            <Route path="*" element={<NotFoundPage page={'404'} />} />
-          </Route>
-        </Routes>
-      </>
-    );
-  }
-}
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage page={'Main'} />} />
+          <Route path="about" element={<AboutUsPage page={'About'} />} />
+          <Route path="form" element={<FormPage page={'Form'} />} />
+          <Route path="*" element={<NotFoundPage page={'404'} />} />
+        </Route>
+      </Routes>
+    </>
+  );
+};
 
 export default App;
