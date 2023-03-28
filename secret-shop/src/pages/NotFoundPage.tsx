1dac1a8e9d1b01118bc24aecf30fe4a6d8a09d91
Timothy7310 <penlist127@gmail.com>
2023-03-28 16:48:32 +0300

    refactor: rewrite class component NotFoundPage to func

--- secret-shop/src/pages/NotFoundPage.tsx
+++ secret-shop/src/pages/NotFoundPage.tsx
@@ -3,23 +3,17 @@ import '../styles/blocks/error.scss';
 import Header from '../components/Header';
 import { CurrentPageProps } from '../types/types';
 
-class NotFoundPage extends React.Component<CurrentPageProps> {
-  constructor(props: CurrentPageProps) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <>
-        <Header page={this.props.page} />
-        <main>
-          <section className="error">
-            <h2 className="error__title visually-hidden">Страница не найдена</h2>
-          </section>
-        </main>
-      </>
-    );
-  }
-}
+const NotFoundPage = ({ page }: CurrentPageProps) => {
+  return (
+    <>
+      <Header page={page} />
+      <main>
+        <section className="error">
+          <h2 className="error__title visually-hidden">Страница не найдена</h2>
+        </section>
+      </main>
+    </>
+  );
+};
 
 export default NotFoundPage;
