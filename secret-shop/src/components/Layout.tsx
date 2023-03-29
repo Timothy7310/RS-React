4a7885a2881a207ac04a23da3630cb94558e0429
Timothy7310 <penlist127@gmail.com>
2023-03-29 16:47:12 +0300

    refactor: rewrite class component Layout to func

--- secret-shop/src/components/Layout.tsx
+++ secret-shop/src/components/Layout.tsx
@@ -1,16 +1,13 @@
-import React from 'react';
 import Footer from './Footer';
 import { Outlet } from 'react-router-dom';
 
-class Layout extends React.Component {
-  render() {
-    return (
-      <>
-        <Outlet />
-        <Footer />
-      </>
-    );
-  }
-}
+const Layout = () => {
+  return (
+    <>
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
 export default Layout;
