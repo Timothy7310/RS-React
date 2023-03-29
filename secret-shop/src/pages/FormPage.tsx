1da9d1dd29d2b91bcf871cc5af0b20a906c78fe6
Timothy7310 <penlist127@gmail.com>
2023-03-29 16:42:08 +0300

    refactor: rewrite class component FormPage to func

--- secret-shop/src/pages/FormPage.tsx
+++ secret-shop/src/pages/FormPage.tsx
@@ -1,40 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 import { CurrentPageProps, FormCard } from '../types/types';
 import Form from '../components/Form/Form';
 import '../styles/blocks/form-page.scss';
 import CardFormList from '../components/Card/CardFormList';
 
-class FormPage extends React.Component<CurrentPageProps, { cards: FormCard[] }> {
-  constructor(props: CurrentPageProps) {
-    super(props);
-    this.state = {
-      cards: [],
-    };
-    this.addCard = this.addCard.bind(this);
-  }
+const FormPage = ({ page }: CurrentPageProps) => {
+  const [cards, setCard] = useState<FormCard[]>([]);
 
-  addCard(card: FormCard) {
-    this.setState((formPageState) => {
-      return { cards: [...formPageState.cards, card] };
-    });
-  }
-
-  render() {
-    return (
-      <>
-        <Header page={this.props.page} />
-        <main>
-          <div className="form-page">
-            <div className="form-page__wrap container">
-              <Form cardValue={(value) => this.addCard(value)} />
-              {this.state.cards.length > 0 && <CardFormList cards={this.state.cards} />}
-            </div>
+  return (
+    <>
+      <Header page={page} />
+      <main>
+        <div className="form-page">
+          <div className="form-page__wrap container">
+            <Form cardValue={(value) => setCard([...cards, value])} />
+            {cards.length > 0 && <CardFormList cards={cards} />}
           </div>
-        </main>
-      </>
-    );
-  }
-}
+        </div>
+      </main>
+    </>
+  );
+};
 
 export default FormPage;
