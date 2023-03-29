489a8aa11e81c9fa4554156a576e4ae52dbfaace
Timothy7310 <penlist127@gmail.com>
2023-03-29 16:44:30 +0300

    refactor: rewrite class component CurrentPage to func

--- secret-shop/src/components/CurrentPage.tsx
+++ secret-shop/src/components/CurrentPage.tsx
@@ -1,17 +1,11 @@
-import React from 'react';
 import { CurrentPageProps } from '../types/types';
 
-class CurrentPage extends React.Component<CurrentPageProps> {
-  constructor(props: CurrentPageProps) {
-    super(props);
-  }
-  render() {
-    return (
-      <>
-        <h2>Current Page: {this.props.page}</h2>
-      </>
-    );
-  }
-}
+const CurrentPage = ({ page }: CurrentPageProps) => {
+  return (
+    <>
+      <h2>Current Page: {page}</h2>
+    </>
+  );
+};
 
 export default CurrentPage;
