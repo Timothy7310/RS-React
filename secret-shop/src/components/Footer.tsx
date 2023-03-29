adcb00cb88b59a8f51dc125bd105ba40a27b989b
Timothy7310 <penlist127@gmail.com>
2023-03-29 16:45:46 +0300

    refactor: rewrite class component Footer to func

--- secret-shop/src/components/Footer.tsx
+++ secret-shop/src/components/Footer.tsx
@@ -1,22 +1,19 @@
-import React from 'react';
 import rssLogo from '../assets/img/rss-logo.svg';
 import '../styles/blocks/footer.scss';
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <footer className="footer">
-        <div className="footer__wrap container">
-          <div className="footer__info">
-            <span className="footer__info-year">Year of creation: 2023</span>
-          </div>
-          <a href="https://rs.school/js/" className="footer__rss">
-            <img src={rssLogo} alt="" />
-          </a>
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__wrap container">
+        <div className="footer__info">
+          <span className="footer__info-year">Year of creation: 2023</span>
         </div>
-      </footer>
-    );
-  }
-}
+        <a href="https://rs.school/js/" className="footer__rss">
+          <img src={rssLogo} alt="" />
+        </a>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
