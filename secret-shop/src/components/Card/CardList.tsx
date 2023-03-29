1eb8a398cce8a01b092640df3477a090a02ff58f
Timothy7310 <penlist127@gmail.com>
2023-03-29 18:36:41 +0300

    refactor: rewrite class component CardList to func

--- secret-shop/src/components/Card/CardList.tsx
+++ secret-shop/src/components/Card/CardList.tsx
@@ -4,20 +4,18 @@ import { CardListProps } from 'types/types';
 import '../../styles/blocks/card-list.scss';
 import Search from '../Search';
 
-class CardList extends React.Component<CardListProps> {
-  render() {
-    return (
-      <div className="cards">
-        <h2 className="visually-hidden">Товары</h2>
-        <Search />
-        <ul className="goods-cards__list goods-cards__list--4">
-          {this.props.cards.map((card) => (
-            <CardItem card={card} key={card.id} />
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+const CardList = ({ cards }: CardListProps) => {
+  return (
+    <div className="cards">
+      <h2 className="visually-hidden">Товары</h2>
+      <Search />
+      <ul className="goods-cards__list goods-cards__list--4">
+        {cards.map((card) => (
+          <CardItem card={card} key={card.id} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default CardList;
