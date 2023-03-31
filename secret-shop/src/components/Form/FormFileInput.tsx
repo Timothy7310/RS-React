647682b7e465251dda415ec533888206601c0971
Timothy7310 <penlist127@gmail.com>
2023-03-31 18:55:40 +0300

    refactor: rewrite class component FormFileInput to func

--- secret-shop/src/components/Form/FormFileInput.tsx
+++ secret-shop/src/components/Form/FormFileInput.tsx
@@ -1,34 +1,29 @@
-import React from 'react';
-import { FormElementsProps, FormTextInputProps } from '../../types/types';
 import FormErrorMessage from './FormErrorMessage';
+import { FormInputProps, ValidationSchemaFileInput } from '../../types/types';
 
-class FormFileInput extends React.Component<FormElementsProps & FormTextInputProps> {
-  fileInputRef: React.RefObject<HTMLInputElement>;
-
-  constructor(props: FormElementsProps & FormTextInputProps) {
-    super(props);
-    this.fileInputRef = React.createRef();
-  }
-
-  render() {
-    return (
-      <div className="form__element-wrap">
-        <label htmlFor={this.props.id} className="form__element-label">
-          {this.props.label}
-        </label>
-        <input
-          type="file"
-          accept="image/png, image/jpeg"
-          name={this.props.name}
-          ref={this.fileInputRef}
-          id={this.props.id}
-          className="form__element-input"
-          required
-        />
-        <FormErrorMessage valid={this.props.valid} message="Ops some mistake! try harder" />
-      </div>
-    );
-  }
-}
+const FormFileInput = ({
+  name,
+  id,
+  label,
+  register,
+  validationSchema,
+  errors,
+}: FormInputProps & ValidationSchemaFileInput) => {
+  return (
+    <div className="form__element-wrap">
+      <label htmlFor={id} className="form__element-label">
+        {label}
+      </label>
+      <input
+        {...register(name, validationSchema)}
+        type="file"
+        accept="image/png, image/jpeg"
+        id={id}
+        className="form__element-input"
+      />
+      {errors?.[name] && <FormErrorMessage message={errors?.[name]?.message || 'Errors'} />}
+    </div>
+  );
+};
 
 export default FormFileInput;
