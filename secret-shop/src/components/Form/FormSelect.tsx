871d0e938e2d867235af5577e8be6dbd3cc96759
Timothy7310 <penlist127@gmail.com>
2023-03-31 18:56:59 +0300

    refactor: rewrite class component FormSelect to func

--- secret-shop/src/components/Form/FormSelect.tsx
+++ secret-shop/src/components/Form/FormSelect.tsx
@@ -1,42 +1,33 @@
-import React from 'react';
-import { FormElementsProps, FormTextInputProps } from '../../types/types';
-import rarityOptions from '../../assets/rarityOptions';
 import FormErrorMessage from './FormErrorMessage';
+import rarityOptions from '../../assets/rarityOptions';
+import { FormInputProps, ValidationSchemaSelect } from '../../types/types';
 
-class FormSelect extends React.Component<FormElementsProps & FormTextInputProps> {
-  selectRef: React.RefObject<HTMLSelectElement>;
-
-  constructor(props: FormElementsProps & FormTextInputProps) {
-    super(props);
-    this.selectRef = React.createRef();
-  }
-
-  render() {
-    return (
-      <div className="form__element-wrap">
-        <label htmlFor={this.props.id} className="form__element-label">
-          {this.props.label}
-        </label>
-        <select
-          ref={this.selectRef}
-          name={this.props.name}
-          id={this.props.id}
-          className="form__element-input"
-          required
-        >
-          <option value="" hidden>
-            Choose one
+const FormSelect = ({
+  name,
+  id,
+  label,
+  register,
+  validationSchema,
+  errors,
+}: FormInputProps & ValidationSchemaSelect) => {
+  return (
+    <div className="form__element-wrap">
+      <label htmlFor={id} className="form__element-label">
+        {label}
+      </label>
+      <select id={id} className="form__element-input" {...register(name, validationSchema)}>
+        <option value="" hidden>
+          Choose one
+        </option>
+        {rarityOptions.map((x) => (
+          <option key={x.id} value={x.name}>
+            {x.name}
           </option>
-          {rarityOptions.map((x) => (
-            <option key={x.id} value={x.name}>
-              {x.name}
-            </option>
-          ))}
-        </select>
-        <FormErrorMessage valid={this.props.valid} message="Ops some mistake! try harder" />
-      </div>
-    );
-  }
-}
+        ))}
+      </select>
+      {errors?.[name] && <FormErrorMessage message={errors?.[name]?.message || 'Errors'} />}
+    </div>
+  );
+};
 
 export default FormSelect;
