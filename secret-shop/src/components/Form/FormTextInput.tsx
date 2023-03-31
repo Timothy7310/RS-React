93a8f9c394e2d135d478e3fed243cddc38f54f1e
Timothy7310 <penlist127@gmail.com>
2023-03-31 18:54:42 +0300

    refactor: rewrite class component FormTextInput to func

--- secret-shop/src/components/Form/FormTextInput.tsx
+++ secret-shop/src/components/Form/FormTextInput.tsx
@@ -1,33 +1,28 @@
-import React from 'react';
-import { FormElementsProps, FormTextInputProps } from '../../types/types';
 import FormErrorMessage from './FormErrorMessage';
+import { FormInputProps, ValidationSchemaTextInput } from '../../types/types';
 
-class FormTextInput extends React.Component<FormElementsProps & FormTextInputProps> {
-  textInputRef: React.RefObject<HTMLInputElement>;
-
-  constructor(props: FormElementsProps & FormTextInputProps) {
-    super(props);
-    this.textInputRef = React.createRef();
-  }
-
-  render() {
-    return (
-      <div className="form__element-wrap">
-        <label htmlFor={this.props.id} className="form__element-label">
-          {this.props.label}
-        </label>
-        <input
-          type="text"
-          required
-          name={this.props.name}
-          ref={this.textInputRef}
-          id={this.props.id}
-          className="form__element-input"
-        />
-        <FormErrorMessage valid={this.props.valid} message="Ops some mistake! try harder" />
-      </div>
-    );
-  }
-}
+const FormTextInput = ({
+  name,
+  id,
+  label,
+  register,
+  validationSchema,
+  errors,
+}: FormInputProps & ValidationSchemaTextInput) => {
+  return (
+    <div className="form__element-wrap">
+      <label htmlFor={id} className="form__element-label">
+        {label}
+      </label>
+      <input
+        {...register(name, validationSchema)}
+        type="text"
+        id={id}
+        className="form__element-input"
+      />
+      {errors?.[name] && <FormErrorMessage message={errors?.[name]?.message || 'Errors'} />}
+    </div>
+  );
+};
 
 export default FormTextInput;
