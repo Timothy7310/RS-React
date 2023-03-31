3f4e6d7337eb6061cf8f27fd98bb1a9fb517c516
Timothy7310 <penlist127@gmail.com>
2023-03-31 18:56:06 +0300

    refactor: rewrite class component FormRadio to func

--- secret-shop/src/components/Form/FormRadio.tsx
+++ secret-shop/src/components/Form/FormRadio.tsx
@@ -1,32 +1,27 @@
-import React from 'react';
-import { FormTextInputProps } from '../../types/types';
+import { FormInputProps, ValidationSchemaRadio } from '../../types/types';
 
-class FormRadio extends React.Component<FormTextInputProps> {
-  radioRef: React.RefObject<HTMLInputElement>;
-
-  constructor(props: FormTextInputProps) {
-    super(props);
-    this.radioRef = React.createRef();
-  }
-
-  render() {
-    return (
-      <div className="goods-filter-radio">
-        <label className="goods-filter-radio__label">
-          {this.props.label}
-          <input
-            className="goods-filter-radio__input goods-filter-input"
-            type="radio"
-            name={this.props.name}
-            value={this.props.label}
-            ref={this.radioRef}
-            required
-          />
-          <span className="goods-filter-radio__checkmark"></span>
-        </label>
-      </div>
-    );
-  }
-}
+const FormRadio = ({
+  name,
+  id,
+  label,
+  register,
+  validationSchema,
+}: FormInputProps & ValidationSchemaRadio) => {
+  return (
+    <div className="goods-filter-radio">
+      <label className="goods-filter-radio__label">
+        {label}
+        <input
+          className="goods-filter-radio__input goods-filter-input"
+          type="radio"
+          value={label}
+          id={id}
+          {...register(name, validationSchema)}
+        />
+        <span className="goods-filter-radio__checkmark"></span>
+      </label>
+    </div>
+  );
+};
 
 export default FormRadio;
