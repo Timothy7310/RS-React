247fcbea4655773661b076dbfe79ac833e80548c
Timothy7310 <penlist127@gmail.com>
2023-03-31 18:53:03 +0300

    refactor: rewrite class component FormCheckboxWrap to func

--- secret-shop/src/components/Form/FormChekboxWrap.tsx
+++ secret-shop/src/components/Form/FormChekboxWrap.tsx
@@ -1,18 +1,21 @@
-import React, { HTMLAttributes, DetailedHTMLProps } from 'react';
+import { HTMLAttributes, DetailedHTMLProps } from 'react';
 import FormErrorMessage from './FormErrorMessage';
 
-class FormChekboxWrap extends React.Component<
-  DetailedHTMLProps<HTMLAttributes<HTMLDivElement> & { valid: boolean }, HTMLDivElement>
-> {
-  render() {
-    return (
-      <div className="checkboxs">
-        <span className="checkboxs-title">Show Elements</span>
-        <div className="checkboxs-wrap">{this.props.children}</div>
-        <FormErrorMessage valid={this.props.valid} message="Ops some mistake! try harder" />
-      </div>
-    );
-  }
-}
+const FormChekboxWrap = ({
+  valid,
+  message,
+  children,
+}: DetailedHTMLProps<
+  HTMLAttributes<HTMLDivElement> & { valid: boolean; message: string },
+  HTMLDivElement
+>) => {
+  return (
+    <div className="checkboxs">
+      <span className="checkboxs-title">Show Elements</span>
+      <div className="checkboxs-wrap">{children}</div>
+      {valid && <FormErrorMessage message={message} />}
+    </div>
+  );
+};
 
 export default FormChekboxWrap;
