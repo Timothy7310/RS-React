e3b7e68acac764b6ecbd2733ac80bfd0b9467b4b
Timothy7310 <penlist127@gmail.com>
2023-03-31 18:51:47 +0300

    refactor: rewrite class component FormCheckbox to func

--- secret-shop/src/components/Form/FormCheckbox.tsx
+++ secret-shop/src/components/Form/FormCheckbox.tsx
@@ -1,31 +1,20 @@
-import React from 'react';
-import { FormTextInputProps } from '../../types/types';
+import { FormInputProps } from '../../types/types';
 
-class FormCheckbox extends React.Component<FormTextInputProps> {
-  checkboxRef: React.RefObject<HTMLInputElement>;
-
-  constructor(props: FormTextInputProps) {
-    super(props);
-    this.checkboxRef = React.createRef();
-  }
-
-  render() {
-    return (
-      <div className="goods-filter-checkbox">
-        <label className="goods-filter-checkbox__label">
-          {this.props.label}
-          <input
-            type="checkbox"
-            name={this.props.name}
-            value={this.props.label}
-            className="goods-filter-checkbox__input goods-filter-input"
-            ref={this.checkboxRef}
-          />
-          <span className="goods-filter-checkbox__checkmark"></span>
-        </label>
-      </div>
-    );
-  }
-}
+const FormCheckbox = ({ name, id, label, register }: FormInputProps) => {
+  return (
+    <div className="goods-filter-checkbox">
+      <label className="goods-filter-checkbox__label">
+        {label}
+        <input
+          {...register(name)}
+          id={id}
+          type="checkbox"
+          className="goods-filter-checkbox__input goods-filter-input"
+        />
+        <span className="goods-filter-checkbox__checkmark"></span>
+      </label>
+    </div>
+  );
+};
 
 export default FormCheckbox;
