f1184b89235ce6f3c5f6fb821e6cc05632be0961
timothy <penlist127@gmail.com>
2023-04-01 17:01:52 +0300

    refactor: rewrite test for formNumberInput with new form logic

--- secret-shop/test/formNumberInput.test.tsx
+++ secret-shop/test/formNumberInput.test.tsx
@@ -1,13 +1,40 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import FormNumberInput from '../src/components/form/FormNumberInput';
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
+const FormNumberInputWrap = () => {
+  const {
+    register,
+    formState: { errors },
+  } = useForm({
+    mode: 'onSubmit',
+  });
+
+  return (
+    <FormNumberInput
+      name="price"
+      id="formItemPrice"
+      label="Price"
+      register={register}
+      validationSchema={{
+        required: 'Required to fill in 👺',
+        min: {
+          value: 1,
+          message: 'Must be greater than 0 💰',
+        },
+      }}
+      errors={errors}
+    />
+  );
+};
+
 describe('FormNumberInput component', () => {
   it('render', async () => {
     const user = userEvent.setup();
-    render(<FormNumberInput label="Price" name="price" id="formItemPrice" valid={false} />);
+    render(<FormNumberInputWrap />);
     const input = screen.getByLabelText('Price') as HTMLInputElement;
     await user.type(input, '5');
     expect(input.value).toBe('5');
