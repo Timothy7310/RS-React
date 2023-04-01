05af9ffd406ed68d8e2d760949881112d6ef8808
timothy <penlist127@gmail.com>
2023-04-01 17:01:13 +0300

    refactor: rewrite test for formSelect with new form logic

--- secret-shop/test/formSelect.test.tsx
+++ secret-shop/test/formSelect.test.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import FormSelect from '../src/components/form/FormSelect';
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
+const FormSelectWrap = () => {
+  const {
+    register,
+    formState: { errors },
+  } = useForm({
+    mode: 'onSubmit',
+  });
+
+  return (
+    <FormSelect
+      name="rarity"
+      id="formRarity"
+      label="Rarity"
+      register={register}
+      validationSchema={{
+        required: 'Required to fill in 👺',
+      }}
+      errors={errors}
+    />
+  );
+};
+
 describe('FormSelect component', () => {
   it('select option', async () => {
     const user = userEvent.setup();
-    render(<FormSelect label="Rarity" name="rarity" id="formRarity" valid={false} />);
+    render(<FormSelectWrap />);
     const select = screen.getByLabelText('Rarity') as HTMLSelectElement;
     await user.selectOptions(select, 'arcana');
     expect(select.value).toBe('arcana');
