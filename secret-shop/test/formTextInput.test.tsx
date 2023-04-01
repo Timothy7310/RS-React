8eda9e0d86cb005702b2d8ba9f28886307ae47dc
timothy <penlist127@gmail.com>
2023-04-01 17:00:21 +0300

    refactor: rewrite test for formTextInput with new form logic

--- secret-shop/test/formTextInput.test.tsx
+++ secret-shop/test/formTextInput.test.tsx
@@ -1,15 +1,42 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import FormTextInput from '../src/components/form/FormTextInput';
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 
+const FormTextInputWrap = () => {
+  const {
+    register,
+    formState: { errors },
+  } = useForm({
+    mode: 'onSubmit',
+  });
+
+  return (
+    <FormTextInput
+      name="itemName"
+      id="itemName"
+      label="Item Name"
+      register={register}
+      validationSchema={{
+        required: 'Required to fill in 👺',
+        minLength: {
+          value: 10,
+          message: 'Minimum of 10 characters 💔',
+        },
+      }}
+      errors={errors}
+    />
+  );
+};
+
 describe('FormTextInput component', () => {
   it('typing', async () => {
     const user = userEvent.setup();
-    render(<FormTextInput label="Item name" name="itemName" id="formItemName" valid={false} />);
-    const input = screen.getByLabelText('Item name') as HTMLInputElement;
+    render(<FormTextInputWrap />);
+    const input = screen.getByLabelText('Item Name') as HTMLInputElement;
     await user.type(input, 'qwerty');
     expect(input.value).toBe('qwerty');
   });
