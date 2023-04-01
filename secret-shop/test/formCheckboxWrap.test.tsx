55f12cea58b635fe57f14b6854f14a689a027ff7
timothy <penlist127@gmail.com>
2023-04-01 17:30:05 +0300

    feat: add test for FormCheckboxWrap component

---
+++ secret-shop/test/formCheckboxWrap.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FormChekboxWrap from '../src/components/Form/FormChekboxWrap';
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('CardFormList component', () => {
+  it('render', () => {
+    render(<FormChekboxWrap valid={false} message={''}></FormChekboxWrap>);
+    const checkboxWrap = screen.getByText('Show Elements');
+    expect(checkboxWrap).toBeInTheDocument();
+  });
+});
