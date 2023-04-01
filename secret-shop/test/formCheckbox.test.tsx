ed3b7b69441d927f129ee65b26ac708f2c365653
timothy <penlist127@gmail.com>
2023-04-01 17:24:04 +0300

    feat: add test for FormCheckbox component

---
+++ secret-shop/test/formCheckbox.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import FormCheckbox from '../src/components/Form/FormCheckbox';
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+const FormCheckboxWrap = () => {
+  const { register } = useForm({
+    mode: 'onSubmit',
+  });
+
+  return <FormCheckbox name="hasBuy" id="hasBuy" label="Buy button" register={register} />;
+};
+
+describe('CardFormList component', () => {
+  it('render', () => {
+    render(<FormCheckboxWrap />);
+    const checkbox = screen.getByLabelText('Buy button');
+    expect(checkbox).toBeInTheDocument();
+  });
+});
