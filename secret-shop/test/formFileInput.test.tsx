09ccac1b8090b91845963ed46fed1819363cb7d5
timothy <penlist127@gmail.com>
2023-04-01 17:03:13 +0300

    refactor: rewrite test for formFileInput with new form logic

--- secret-shop/test/formFileInput.test.tsx
+++ secret-shop/test/formFileInput.test.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import FormFileInput from '../src/components/form/FormFileInput';
 import { describe, it, expect } from 'vitest';
 import { render, screen, fireEvent } from '@testing-library/react';
 
+const FormFileInputWrap = () => {
+  const {
+    register,
+    formState: { errors },
+  } = useForm({
+    mode: 'onSubmit',
+  });
+
+  return (
+    <FormFileInput
+      name="itemImage1"
+      label="Item image"
+      id="itemImage"
+      register={register}
+      validationSchema={{
+        required: 'Required to fill in 👺',
+        validate: {
+          typeImg: (value) =>
+            (value.length > 0 && value[0].type.startsWith('image')) || 'Must be an image 🖼️',
+        },
+      }}
+      errors={errors}
+    />
+  );
+};
+
 describe('FormSelect component', () => {
   it('load file', () => {
-    render(<FormFileInput label="Item image" name="itemImage" id="itemImage" valid={false} />);
+    render(<FormFileInputWrap />);
     const itemImage = new File(['arcana'], 'arcana.jpg', { type: 'image/jpg' });
     const fileInput = screen.getByLabelText('Item image') as HTMLInputElement;
 
@@ -13,9 +40,5 @@ describe('FormSelect component', () => {
     const fileImage = fileInput.files ? fileInput.files[0]?.name : '';
     expect(fileImage).toBe('arcana.jpg');
     expect(fileInput.files).toHaveLength(1);
-  });
-  it('render with error', async () => {
-    render(<FormFileInput label="Item image" name="itemImage" id="itemImage" valid={true} />);
-    expect(screen.getByText('Ops some mistake! try harder'));
   });
 });
