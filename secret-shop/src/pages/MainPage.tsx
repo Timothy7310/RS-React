d861b6fc83a1ca0cefee09e5e772518a1891d950
Timothy7310 <penlist127@gmail.com>
2023-04-05 18:35:28 +0300

    refactor: add function for searchMovie & top250 movie

--- secret-shop/src/pages/MainPage.tsx
+++ secret-shop/src/pages/MainPage.tsx
@@ -3,7 +3,7 @@ import '../styles/blocks/goods.scss';
 import Header from '../components/Header';
 import { CurrentPageProps, Cards } from '../types/types';
 import { useState, useEffect } from 'react';
-import axiosInstance from '../services/api';
+import { axiosInstance, searchMovie, getTop250 } from '../services/api';
 import CardLoader from '../components/Card/CardLoader';
 import Search from '../components/Search';
 
@@ -17,21 +17,14 @@ const MainPage = ({ page }: CurrentPageProps) => {
       try {
         if (searchValue) {
           setIsLoad(false);
-          await axiosInstance
-            .get(`movie?token=4ZKP5GE-A9D47VB-HQ3453N-8WCS1CM&page=1&limit=12&name=${searchValue}`)
-            .then((response) => {
-              setCards([...response.data.docs]);
-              setIsLoad(true);
-            });
+          const response = await searchMovie(searchValue);
+          setCards([...response.data.docs]);
         } else {
           setIsLoad(false);
-          await axiosInstance
-            .get(`movie?token=4ZKP5GE-A9D47VB-HQ3453N-8WCS1CM&page=1&limit=12&top250=%21null`)
-            .then((response) => {
-              setCards([...response.data.docs]);
-              setIsLoad(true);
-            });
+          const response = await getTop250();
+          setCards([...response.data.docs]);
         }
+        setIsLoad(true);
       } catch (err) {
         console.log(err);
       }
--- secret-shop/src/services/api.ts
+++ secret-shop/src/services/api.ts
@@ -1,8 +1,22 @@
 import axios from 'axios';
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 const axiosInstance = axios.create({
   baseURL: 'https://api.kinopoisk.dev/v1/',
   timeout: 5000,
+  headers: {
+    'X-API-KEY': API_KEY,
+  },
 });
 
-export default axiosInstance;
+const searchMovie = async (searchValue: string) => {
+  const response = await axiosInstance.get(`movie?&page=1&limit=12&name=${searchValue}`);
+  return response;
+};
+
+const getTop250 = async () => {
+  const response = await axiosInstance.get(`movie?&page=1&limit=12&top250=%21null`);
+  return response;
+};
+
+export { axiosInstance, searchMovie, getTop250 };
