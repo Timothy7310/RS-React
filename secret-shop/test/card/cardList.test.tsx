01c4c1c2b367b6501d7401d35098c3f683322865
timothy <penlist127@gmail.com>
2023-04-09 11:26:26 +0300

    feat: add req searchMovie for test CardList component

--- secret-shop/test/card/cardList.test.tsx
+++ secret-shop/test/card/cardList.test.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import CardList from '../../src/components/Card/CardList';
-import { describe, it } from 'vitest';
-import { render } from '@testing-library/react';
-import goods from '../../src/assets/goods.json';
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { searchMovie } from '../../src/services/api';
 
 describe('CardList component', () => {
-  it('render', () => {
-    render(<CardList cards={goods} />);
+  it('render', async () => {
+    const movies = await searchMovie('Зеленая миля');
+
+    render(<CardList cards={movies.data.docs} />);
+    expect(screen.getByText('Зеленая миля'));
   });
 
   it('render empty', () => {
